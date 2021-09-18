bd55b93de06c40a2b6d009cf3135052e2772cb86
Robert Rega <robertdrega@gmail.com>
2021-09-18 15:14:45 -0400

    added model and integrated with blog

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
 
-from datetime import date
-
-all_posts = [
-  {
-    "slug": "hike-in-the-mountains",
-    "image": "mountains.jpg",
-    "author": "Robbie",
-    "date": date(2021, 7, 21),
-    "title": "Mountin Hiking",
-    "excerpt": "There's nothing...",
-    "content": "More details here",
-  }
-]
-
-def get_date(post):
-  return post["date"]
+from .models import Post
 
 # Create your views here.
 def starting_page(request):
-  sorted_posts = sorted(all_posts, key=get_date)
-  latest_posts = sorted_posts[-3:]
+  latest_posts = Post.objects.all().order_by("-date")[:3]
   return render(request, "blog/index.html", {
     "posts": latest_posts,
 
   })
 
 def posts(request):
+  all_posts = Post.objects.all().order_by("-date")
   return render(request, "blog/all-posts.html", {
     "all_posts": all_posts
   })
 
 def post_detail(request, slug):
-  identified_post = next(post for post in all_posts if post['slug'] == slug)
+  identified_post = get_object_or_404(Post, slug=slug)
   return render(request, "blog/post-detail.html", {
-    "post": identified_post
+    "post": identified_post,
+    "post_tags": identified_post.tags.all()
   })
