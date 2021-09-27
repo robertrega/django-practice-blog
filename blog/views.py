b92998f57ff59e9f4dfc70ccc067edd23677da94
Robert Rega <robertdrega@gmail.com>
2021-09-26 23:02:05 -0400

    comment post finished

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.view import View
 
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class StartingPageView(ListView):
@@ -21,11 +25,30 @@ class AllPostsView(ListView):
   ordering = ["-date"]
   context_object_name = "all_posts"
 
-class SinglePostView(DetailView):
-  template_name = "blog/post-detail.html"
-  model = Post
+class SinglePostView(View):
+  def get(self, request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+      "post": post,
+      "post_tags": post.tags.all(),
+      "comment_form": CommentForm(),
+    }
+    return render(request, "blog/post-detail.html", context)
 
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["post_tags"] = self.object.tags.all()
-      return context
+  def post(self, request, slug):
+    comment_form = CommentForm(request.POST)
+    post = Post.objects.get(slug=slug)
+
+    if comment_form.is_valid():
+      comment = comment_form.save(commit=False)
+      comment.post = post
+      comment.save()
+      
+      return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+    
+    context = {
+      "post": post,
+      "post_tags": post.tags.all(),
+      "comment_form": CommentForm(),
+    }
+    return render(request, "blog/post-detail.html", context)
