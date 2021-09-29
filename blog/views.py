059529dd1481713282a6c5236b012928be3f9d25
Robert Rega <robertdrega@gmail.com>
2021-09-28 21:41:54 -0400

    comments view added

--- blog/views.py
+++ blog/views.py
@@ -32,6 +32,7 @@ class SinglePostView(View):
       "post": post,
       "post_tags": post.tags.all(),
       "comment_form": CommentForm(),
+      "comments": post.comments.all().order_by("-id"),
     }
     return render(request, "blog/post-detail.html", context)
 
@@ -50,5 +51,6 @@ class SinglePostView(View):
       "post": post,
       "post_tags": post.tags.all(),
       "comment_form": CommentForm(),
+      "comments": post.comments.all().order_by("-id"),
     }
     return render(request, "blog/post-detail.html", context)
